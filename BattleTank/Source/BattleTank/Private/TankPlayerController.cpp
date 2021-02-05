036bf6585dee1e5ccce543dc261918bd7c3a014e
Lisa Buccheri <lisa.buccheri@hotmail.com>
2021-02-05 20:32:02 +0100

    Player controller C++
    Create player controller C++ class. Initiate an override function for BeginPlay and Tick

---
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+//#include "BattleTank.h"
+#include "TankPlayerController.h"
+
+
+
+void ATankPlayerController::BeginPlay() 
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank) 
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController not possessing tank "));
+
+	}
+	else 
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController possessing:  %s"), *(ControlledTank->GetName()));
+
+	}
+}
+
+void ATankPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	
+	AimTowardCrossahair();
+}
+
+ATank* ATankPlayerController::GetControlledTank() const 
+{
+	return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::AimTowardCrossahair()
+{
+	if (!GetControlledTank()) { return; }
+
+	FVector HitLocation; //Out parameter
+	if (GetSightRayHitLocation(HitLocation))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("HitLocation: %s"), *HitLocation.ToString());
+
+	}
+
+
+		//TODO Tell controlled tank to aim at this point
+}
+
+	//Get world location if linetrace through crosshair, true if it hits landscape 
+bool ATankPlayerController::GetSightRayHitLocation(FVector& HitLocation) const
+{
+	HitLocation = FVector(1.0);
+	return true;
+}
+
